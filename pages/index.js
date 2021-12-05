b3d11fa1a995decc33110708ab22bf704b622775
keremduran <keremduran@live.com>
2021-12-04 21:28:11 -0500

    image domain bug fix, bayut api fetch working

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({
   purpose,
@@ -12,7 +14,7 @@ export const Banner = ({
   imageUrl,
 }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <img src={imageUrl} width={500} height={300} />
+    <Image src={imageUrl} width={500} height={300} />
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>
         {purpose}
@@ -36,7 +38,8 @@ export const Banner = ({
   </Flex>
 );
 
-export default function Home() {
+export default function Home(propertiesForRent, propertiesForSale) {
+  console.log((propertiesForRent, propertiesForSale));
   return (
     <Box>
       <Banner
@@ -49,7 +52,14 @@ export default function Home() {
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
-      <Flex flexWrap='wrap'>{/*Fetch properties and map over them...*/}</Flex>
+      {/*Fetch properties and map over them...
+            <Flex flexWrap='wrap'>
+        {propertiesForRent.map((property) => (
+          <Property />
+        ))}
+      </Flex>
+      */}
+
       <Banner
         purpose={'BUY A HOME'}
         title1='Find, Buy, and Own Your'
@@ -63,4 +73,20 @@ export default function Home() {
       <Flex flexWrap='wrap'>{/*Fetch properties and map over them...*/}</Flex>
     </Box>
   );
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
---
+++ pages/utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const baseUrl = 'bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': 'ca2ead922cmsh79dd6c1a31a0182p16e81ajsnce18c3968ecc',
+    },
+  });
+  return data;
+};
