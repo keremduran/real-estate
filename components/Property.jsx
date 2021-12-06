25d291791d6f7365aef409182896d5e1c0b357b6
keremduran <keremduran@live.com>
2021-12-05 20:46:10 -0500

    property card complete

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,52 +1,78 @@
 import Link from 'next/link';
 import Image from 'next/Image';
-import { Box, Flex, Text, Avater } from '@chakra-ui/react';
+import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 import DefaultImage from '../assets/images/defaultImage.jpg';
 const Property = ({
-  property: {
-    coverPhoto,
-    price,
-    rentFrequency,
-    rooms,
-    title,
-    baths,
-    area,
-    agency,
-    isVerified,
-    externalId,
-  },
+    property: {
+        coverPhoto,
+        price,
+        rentFrequency,
+        rooms,
+        title,
+        baths,
+        area,
+        agency,
+        isVerified,
+        externalId,
+    },
 }) => (
-  <Link href={`/property/${externalId}`} passHref>
-    <Flex
-      flexWrap='wrap'
-      w='420px'
-      p='5'
-      paddingTop='0'
-      justifyContent='flex-start'
-      cursor='pointer'
-    >
-      <Box>
-        <img
-          width={400}
-          height={260}
-          src={coverPhoto ? coverPhoto.url : DefaultImage}
-        />
-      </Box>
-      <Box w='full'>
-        <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
-          <Flex alignItems='center'>
-            {' '}
-            {isVerified && <GoVerified />}
-            <Box paddingRight='3' color='green.400'></Box>
-          </Flex>
+    <Link href={`/property/${externalId}`} passHref>
+        <Flex
+            flexWrap="wrap"
+            w="420px"
+            p="5"
+            paddingTop="0"
+            justifyContent="flex-start"
+            cursor="pointer"
+        >
+            <Box>
+                <img
+                    width={400}
+                    height={260}
+                    src={coverPhoto ? coverPhoto.url : DefaultImage}
+                />
+            </Box>
+            <Box w="full">
+                <Flex
+                    paddingTop="2"
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Flex alignItems="center">
+                        <Box paddingRight="3" color="green.400">
+                            {isVerified && <GoVerified />}
+                        </Box>
+                        <Text fontWeight="bold" fontSize="lg">
+                            AED {millify(price)}
+                            {rentFrequency && `/${rentFrequency}`}
+                        </Text>
+                        <Box>
+                            <Avatar size="sm" src={agency?.logo?.url} />
+                        </Box>
+                        <Flex
+                            alignItems="center"
+                            p="1"
+                            justifyContent="space-between"
+                            w="250px"
+                            color="blue.400"
+                        >
+                            {rooms} <FaBed /> | {baths} <FaBath /> |{' '}
+                            {millify(area)} sqft <BsGridFill />
+                            <Text fontSize="lg">
+                                {title.length > 30
+                                    ? `${title.substring(0, 30)}...`
+                                    : title}
+                            </Text>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </Box>
         </Flex>
-      </Box>
-    </Flex>
-  </Link>
+    </Link>
 );
 
 export default Property;
