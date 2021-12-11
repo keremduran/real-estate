d3fcd3767b76c1d147bdcac5046f26f07d702513
keremduran <keremduran@live.com>
2021-12-11 14:45:20 -0500

    search page ui

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import {
+    Flex,
+    Select,
+    Box,
+    Text,
+    Input,
+    Spinner,
+    Icon,
+    Button,
+} from '@chakra-ui/layout';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import { Image } from '@chakra-ui/image';
+
+const SearchFilters = () => {
+    return <div>searchFilters</div>;
+};
+
+export default SearchFilters;
