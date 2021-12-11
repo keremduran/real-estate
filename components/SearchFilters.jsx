8930d8c1bd7a36f1668be168429de787a53d398e
keremduran <keremduran@live.com>
2021-12-11 16:09:33 -0500

    filter menu json, and filter ui setup

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -8,13 +8,30 @@ import {
     Spinner,
     Icon,
     Button,
-} from '@chakra-ui/layout';
-import { useRouter } from 'next/router';
-import { MdCancel } from 'react-icons/md';
-import { Image } from '@chakra-ui/image';
+} from '@chakra-ui/react';
+import { filterData, getFilterValues } from '../utils/filterData';
 
 const SearchFilters = () => {
-    return <div>searchFilters</div>;
+    const [filters, setFilters] = useState(filterData);
+    const searchProperties = (filterValues) => {};
+    return (
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) =>
+                            searchProperties({
+                                [filter.queryName]: e.target.value,
+                            })
+                        }
+                    ></Select>
+                </Box>
+            ))}
+        </Flex>
+    );
 };
 
 export default SearchFilters;
