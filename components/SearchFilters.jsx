7fa90069c1ac96b6498a1621359a0150caf3b393
keremduran <keremduran@live.com>
2021-12-12 17:11:41 -0500

    Options

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -10,10 +10,22 @@ import {
     Button,
 } from '@chakra-ui/react';
 import { filterData, getFilterValues } from '../utils/filterData';
+import router from 'next/router';
 
 const SearchFilters = () => {
     const [filters, setFilters] = useState(filterData);
-    const searchProperties = (filterValues) => {};
+    const searchProperties = (filterValues) => {
+        const pathname = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value;
+        });
+
+        router.push({pathname, query})
+    };
     return (
         <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
             {filters.map((filter) => (
@@ -27,7 +39,14 @@ const SearchFilters = () => {
                                 [filter.queryName]: e.target.value,
                             })
                         }
-                    ></Select>
+                    >
+                        {filter?.items?.map((item) => (
+                            <option key={item.value} value={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                    <Input placeholder='Location' />
                 </Box>
             ))}
         </Flex>
