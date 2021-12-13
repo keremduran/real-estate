647411dadebbc70c27b5a25e40d513fe8675cef9
keremduran <keremduran@live.com>
2021-12-12 20:13:53 -0500

    minor changes to image scrollbar

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import Image from 'next/image';
-import { Box, Icon, Flex, Center } from '@chakra-ui/react';
+import { Box, Icon, Center } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
@@ -32,14 +32,27 @@ const RightArrow = () => {
     );
 };
 
-export default function ImageScrollbar({ data }) {
-  return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }}>
-      {data.map((item) => (
-        <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
-          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
-        </Box>
-      ))}
-    </ScrollMenu>
-  );
-}
+const ImageScrollbar = ({ data }) => {
+    return (
+        <ScrollMenu
+            LeftArrow={LeftArrow}
+            RightArrow={RightArrow}
+            style={{ overflow: 'hidden' }}
+        >
+            {data.map((item) => (
+                <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+                    <Image
+                        placeholder="blur"
+                        blurDataURL={item.url}
+                        src={item.url}
+                        width={1000}
+                        height={500}
+                        sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+                    />
+                </Box>
+            ))}
+        </ScrollMenu>
+    );
+};
+
+export default ImageScrollbar;
