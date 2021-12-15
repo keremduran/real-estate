2ffe0d86864c095d148eb8b56acf9cc004b8fe83
keremduran <keremduran@live.com>
2021-12-15 15:22:26 -0500

    First Deployment on Vercel

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import {Box} from '@chakra-ui/react'
 
 const Footer = () => (
     <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
-        Realtor 2022 all rights reserved.
+        Made by Kerem Duran.
     </Box>
 )
 
